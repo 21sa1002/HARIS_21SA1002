9c6dbf8683770266fa50dc2b24a03e7cf5b0ed61
21sa1002 <152111394+21sa1002@users.noreply.github.com>
2023-12-04 01:23:07 +0700

    Update SQLQuery.sql
    Db update

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,26 +1,40 @@
 -- Membuat Database
 CREATE DATABASE tokoBangunan;
 
--- Menggunakan Database
 USE tokoBangunan;
 
--- Membuat Tabel tbKategori_barang
-CREATE TABLE tbKategori_barang (
-    idKategori VARCHAR(6) PRIMARY KEY,
-    namaKategori VARCHAR(255)
+-- Tabel tbPenjualan
+CREATE TABLE tbPenjualan (
+    idPenjualan VARCHAR(6) PRIMARY KEY,
+    idPesanan INT,
+    namaPelanggan VARCHAR(255),
+    namaBarang VARCHAR(255),
+    jumlah INT,
+    hargaBarang INT,
+    subTotal INT
 );
 
--- Membuat Tabel tbBarang
-CREATE TABLE tbBarang (
+-- Tabel tbBarang
+CREATE TABLE IF NOT EXISTS tbBarang (
     idBarang VARCHAR(6) PRIMARY KEY,
     namaBarang VARCHAR(255),
     hargaBarang INT,
     stokBarang INT,
-    idKategori VARCHAR(6) Foreign key references tbKategori_barang(idKategori)
+    namaKategori VARCHAR(255),
+    idSuplier VARCHAR(6)
 );
 
--- Membuat Tabel tbPelanggan
-CREATE TABLE tbPelanggan (
+-- Tabel detailPenjualan
+CREATE TABLE IF NOT EXISTS detailPenjualan (
+    idPesanan VARCHAR(6),
+    idPenjualan VARCHAR(6),
+    idBarang VARCHAR(6),
+    totalPenjualan INT,
+    totalTerjual INT
+);
+
+-- Tabel tbPelanggan
+CREATE TABLE IF NOT EXISTS tbPelanggan (
     idPelanggan VARCHAR(6) PRIMARY KEY,
     namaPelanggan VARCHAR(255),
     telepon VARCHAR(20),
@@ -28,30 +42,36 @@ CREATE TABLE tbPelanggan (
     alamat VARCHAR(255)
 );
 
--- Membuat Tabel tbPesanan
-CREATE TABLE tbPesanan (
-    idPesanan VARCHAR(6) PRIMARY KEY,
-    idPelanggan VARCHAR(6) Foreign key references tbPelanggan(idPelanggan),
-    tanggalPesanan DATE,
-    totalHarga INT
-);
-
--- Membuat Tabel tbPenjualan
-CREATE TABLE tbPenjualan (
-    idPenjualan VARCHAR(6) PRIMARY KEY,
-    idPesanan VARCHAR(6) Foreign key references tbPesanan(idPesanan),
-    idPelanggan VARCHAR(6) Foreign key references tbPelanggan(idPelanggan),
-    idBarang VARCHAR(6) Foreign key references tbbarang(idBarang),
-    jumlah INT,
-    totalHarga INT
-);
-
--- Membuat Tabel tbKaryawan
-CREATE TABLE tbKaryawan (
-    idKaryawan VARCHAR(6) PRIMARY KEY,
-    username VARCHAR(50),
-    passwd VARCHAR(50),
+-- Tabel tbKaryawan
+CREATE TABLE IF NOT EXISTS tbKaryawan (
+    idKaryawan INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255),
+    password VARCHAR(255),
     namaKaryawan VARCHAR(255)
 );
 
-Insert into tbKaryawan values ('KRY001', 'admin1', 'admin123', 'Samsul')
+-- Tabel pesanan
+CREATE TABLE IF NOT EXISTS pesanan (
+    idPesanan VARCHAR(6) PRIMARY KEY,
+    idPelanggan VARCHAR(6),
+    tanggalPesanan DATE,
+    totalHarga INT,
+    jenisBayar VARCHAR(50),
+    statusPesanan VARCHAR(50)
+);
+
+-- Tabel tbSuplier
+CREATE TABLE IF NOT EXISTS tbSuplier (
+    idSuplier VARCHAR(6) PRIMARY KEY,
+    namaSuplier VARCHAR(255),
+    tipeBarang VARCHAR(255),
+    telepon VARCHAR(20)
+);
+
+-- Tabel kategori_barang
+CREATE TABLE IF NOT EXISTS kategori_barang (
+    idkategori VARCHAR(6) PRIMARY KEY,
+    namaKategori VARCHAR(255),
+    tipeSatuan INT,
+    stokMaks INT
+);
